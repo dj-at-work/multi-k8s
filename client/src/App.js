6fdae08d8d806a79ddee2cf9cce83e183db8471c
Dirk Jönsson <dirk.joensson@brainbell.de>
2021-05-15 13:19:35 +0200

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator!</h1>
-            <Link to="/">Home</Link>&nbsp;
+            <h1 className="App-title">Fib Calculator UPDATED!</h1>
+            <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
